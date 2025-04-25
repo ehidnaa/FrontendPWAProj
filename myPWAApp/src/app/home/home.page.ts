d79a8c81d6477793d5932c441cd445e71d13f873
Mariia Chalyk <90447244+ehidnaa@users.noreply.github.com>
2025-04-25 18:33:21 +0000

    Implemented HTTP request and data display

--- myPWAApp/src/app/home/home.page.ts
+++ myPWAApp/src/app/home/home.page.ts
@@ -2,14 +2,26 @@ import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [IonicModule, RouterModule, FormsModule],
+  imports: [IonicModule, RouterModule, FormsModule, CommonModule],
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss']
 })
 export class HomePage {
   name: string = '';
+  tasks: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  loadTasks() {
+    this.http.get<any[]>('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .subscribe(data => {
+        this.tasks = data;
+      });
+  }
 }
